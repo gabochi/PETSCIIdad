db866f0a207b6fdd2d4aa4a9534ed3a5869ef6b3
gabochi <gabriel.vinazza@gmail.com>
2020-06-19 17:59:39 -0300

    modified:   main.prg

--- main.asm
+++ main.asm
@@ -5,16 +5,16 @@
 	.byte   $32,$30,$36,$34
 	.byte    $29, $00, $00, $00
 	; Ending memory block
-EndBlock125
+EndBlock1
 	org $810
 	; Starting new memory block at $810
 C64Project
 	jmp block1
 t	dc.w	$00
+expression	dc.w	
 i	dc.b	
 j	dc.b	
 k	dc.b	
-expression	dc.w	
 opcode	dc.b	 
 	org opcode+4
 opcodeindex	dc.b	
@@ -223,6 +223,9 @@ block1
 	lda #0
 			sta $d020
 			sta $d021
+	
+; // PETSCIIdad by gabochi, still a lot to improve...
+; //											Screen preparation
 screenmemory =  $fe
 	; ----------
 	; DefineAddressTable address, StartValue, IncrementValue, TableSize
@@ -280,19 +283,6 @@ MainProgram_clearloop7
 	sta $01f4+$400,x
 	sta $02ee+$400,x
 	bne MainProgram_clearloop7
-	
-; //		for k:=0 to 2 do
-; //		begin
-; //		for j:=0 to 2 do
-; //		begin
-; //		screenmemory:= AddressTable(caddr, 4, 4+j*4+k*8);
-; //		for i:=0 to 128 do
-; //		begin
-; //			screenmemory[i+(i/32)*8]:=temparray[i+j*128];
-; //		end;
-; //		end;
-; //		end;
-; //
 MainProgram_while8
 	; Full binary clause
 	; Binary clause: NOTEQUALS
@@ -300,19 +290,20 @@ MainProgram_while8
 	; Compare with pure num / var optimization
 	cmp #$0;keep
 	; BC done
-	beq MainProgram_binaryclausefailed589
-MainProgram_binaryclausesuccess591
+	beq MainProgram_binaryclausefailed631
+MainProgram_binaryclausesuccess633
 	lda #1; success
-	jmp MainProgram_binaryclausefinished590
-MainProgram_binaryclausefailed589
+	jmp MainProgram_binaryclausefinished632
+MainProgram_binaryclausefailed631
 	lda #0 ; failed state
-MainProgram_binaryclausefinished590
+MainProgram_binaryclausefinished632
 	cmp #1
 	beq MainProgram_ConditionalTrueBlock9
 	jmp MainProgram_elsedoneblock11
 MainProgram_ConditionalTrueBlock9
 	
-; // 											"Game loop"
+; // 											"Game" loop begins
+; //											Cursor effect
 	; Assigning single variable : screenmemory
 	; ----------
 	; AddressTable address, xoffset, yoffset
@@ -327,10 +318,10 @@ MainProgram_ConditionalTrueBlock9
 	; Peek
 	ldy opcodeindex
 	lda (screenmemory),y
-MainProgram_rightvarAddSub_var594 = $54
-	sta MainProgram_rightvarAddSub_var594
+MainProgram_rightvarAddSub_var636 = $54
+	sta MainProgram_rightvarAddSub_var636
 	lda #$80
-	eor MainProgram_rightvarAddSub_var594
+	eor MainProgram_rightvarAddSub_var636
 	; Calling storevariable
 	ldy opcodeindex ; optimized, look out for bugs
 	sta (screenmemory),y
@@ -338,10 +329,10 @@ MainProgram_rightvarAddSub_var594 = $54
 	lda #$0
 	; Calling storevariable
 	sta i
-MainProgram_for595
-MainProgram_forLoopFix860
+MainProgram_for637
+MainProgram_forLoopFix902
 	
-; //											Temp array , expression construction
+; //											Read code, evaluate, write temparray
 	; Assigning single variable : expression
 	; integer assignment NodeVar
 	ldy t+1 ; Next one ; optimized, look out for bugs
@@ -353,8 +344,10 @@ MainProgram_forLoopFix860
 	lda #$0
 	; Calling storevariable
 	sta j
-MainProgram_for864
-MainProgram_forLoopFix994
+MainProgram_for906
+MainProgram_forLoopFix1036
+	
+; //											Four-operation code
 	; Assigning single variable : stack
 	; Load Byte array
 	; 8 bit binop
@@ -370,198 +363,198 @@ MainProgram_forLoopFix994
 	; Binary clause Simplified: EQUALS
 	; Compare with pure num / var optimization
 	cmp #$24;keep
-	bne MainProgram_elsedoneblock1001
-MainProgram_ConditionalTrueBlock999
+	bne MainProgram_elsedoneblock1043
+MainProgram_ConditionalTrueBlock1041
 	; Assigning single variable : stack
 	; integer assignment NodeVar
 	ldy t+1 ; Next one ; optimized, look out for bugs
 	lda t
 	; Calling storevariable
 	sta stack
-MainProgram_elseblock1000
-MainProgram_elsedoneblock1001
+MainProgram_elseblock1042
+MainProgram_elsedoneblock1043
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$22;keep
-	bne MainProgram_elsedoneblock1007
-MainProgram_ConditionalTrueBlock1005
+	bne MainProgram_elsedoneblock1049
+MainProgram_ConditionalTrueBlock1047
 	; Assigning single variable : expression
 	; integer assignment NodeVar
 	ldy expression+1 ; Next one ; optimized, look out for bugs
 	lda expression
-MainProgram_tempVarShift_var1012 = $54
-	sta MainProgram_tempVarShift_var1012
-	sty MainProgram_tempVarShift_var1012+1
+MainProgram_tempVarShift_var1054 = $54
+	sta MainProgram_tempVarShift_var1054
+	sty MainProgram_tempVarShift_var1054+1
 	ldx stack ; optimized, look out for bugs
-MainProgram_lblShift1013
-		lsr MainProgram_tempVarShift_var1012+1
-	ror MainProgram_tempVarShift_var1012+0
+MainProgram_lblShift1055
+		lsr MainProgram_tempVarShift_var1054+1
+	ror MainProgram_tempVarShift_var1054+0
 
 	dex
 	cpx #0
-	bne MainProgram_lblShift1013
-	lda MainProgram_tempVarShift_var1012
-	ldy MainProgram_tempVarShift_var1012+1
+	bne MainProgram_lblShift1055
+	lda MainProgram_tempVarShift_var1054
+	ldy MainProgram_tempVarShift_var1054+1
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1006
-MainProgram_elsedoneblock1007
+MainProgram_elseblock1048
+MainProgram_elsedoneblock1049
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$1c;keep
-	bne MainProgram_elsedoneblock1017
-MainProgram_ConditionalTrueBlock1015
+	bne MainProgram_elsedoneblock1059
+MainProgram_ConditionalTrueBlock1057
 	; Assigning single variable : expression
 	; integer assignment NodeVar
 	ldy expression+1 ; Next one ; optimized, look out for bugs
 	lda expression
-MainProgram_tempVarShift_var1022 = $54
-	sta MainProgram_tempVarShift_var1022
-	sty MainProgram_tempVarShift_var1022+1
+MainProgram_tempVarShift_var1064 = $54
+	sta MainProgram_tempVarShift_var1064
+	sty MainProgram_tempVarShift_var1064+1
 	ldx stack ; optimized, look out for bugs
-MainProgram_lblShift1023
-		asl MainProgram_tempVarShift_var1022+0
-	rol MainProgram_tempVarShift_var1022+1
+MainProgram_lblShift1065
+		asl MainProgram_tempVarShift_var1064+0
+	rol MainProgram_tempVarShift_var1064+1
 
 	dex
 	cpx #0
-	bne MainProgram_lblShift1023
-	lda MainProgram_tempVarShift_var1022
-	ldy MainProgram_tempVarShift_var1022+1
+	bne MainProgram_lblShift1065
+	lda MainProgram_tempVarShift_var1064
+	ldy MainProgram_tempVarShift_var1064+1
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1016
-MainProgram_elsedoneblock1017
+MainProgram_elseblock1058
+MainProgram_elsedoneblock1059
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$11;keep
-	bne MainProgram_elsedoneblock1027
-MainProgram_ConditionalTrueBlock1025
+	bne MainProgram_elsedoneblock1069
+MainProgram_ConditionalTrueBlock1067
 	; Assigning single variable : expression
 	; HandleVarBinopB16bit
 	ldy #0 ; ::HandleVarBinopB16bit 0
 	lda stack
-MainProgram_rightvarInteger_var1033 = $54
-	sta MainProgram_rightvarInteger_var1033
-	sty MainProgram_rightvarInteger_var1033+1
+MainProgram_rightvarInteger_var1075 = $54
+	sta MainProgram_rightvarInteger_var1075
+	sty MainProgram_rightvarInteger_var1075+1
 	lda expression+1
-	and MainProgram_rightvarInteger_var1033+1
+	and MainProgram_rightvarInteger_var1075+1
 	tay
 	lda expression
-	and MainProgram_rightvarInteger_var1033
-	bcs MainProgram_wordAdd1032
+	and MainProgram_rightvarInteger_var1075
+	bcs MainProgram_wordAdd1074
 	dey
-MainProgram_wordAdd1032
+MainProgram_wordAdd1074
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1026
-MainProgram_elsedoneblock1027
+MainProgram_elseblock1068
+MainProgram_elsedoneblock1069
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$1f;keep
-	bne MainProgram_elsedoneblock1037
-MainProgram_ConditionalTrueBlock1035
+	bne MainProgram_elsedoneblock1079
+MainProgram_ConditionalTrueBlock1077
 	; Assigning single variable : expression
 	; HandleVarBinopB16bit
 	ldy #0 ; ::HandleVarBinopB16bit 0
 	lda stack
-MainProgram_rightvarInteger_var1043 = $54
-	sta MainProgram_rightvarInteger_var1043
-	sty MainProgram_rightvarInteger_var1043+1
+MainProgram_rightvarInteger_var1085 = $54
+	sta MainProgram_rightvarInteger_var1085
+	sty MainProgram_rightvarInteger_var1085+1
 	lda expression+1
-	ora MainProgram_rightvarInteger_var1043+1
+	ora MainProgram_rightvarInteger_var1085+1
 	tay
 	lda expression
-	ora MainProgram_rightvarInteger_var1043
-	bcs MainProgram_wordAdd1042
+	ora MainProgram_rightvarInteger_var1085
+	bcs MainProgram_wordAdd1084
 	dey
-MainProgram_wordAdd1042
+MainProgram_wordAdd1084
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1036
-MainProgram_elsedoneblock1037
+MainProgram_elseblock1078
+MainProgram_elsedoneblock1079
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$28;keep
-	bne MainProgram_elsedoneblock1047
-MainProgram_ConditionalTrueBlock1045
+	bne MainProgram_elsedoneblock1089
+MainProgram_ConditionalTrueBlock1087
 	; Assigning single variable : expression
 	; HandleVarBinopB16bit
 	ldy #0 ; ::HandleVarBinopB16bit 0
 	lda stack
-MainProgram_rightvarInteger_var1053 = $54
-	sta MainProgram_rightvarInteger_var1053
-	sty MainProgram_rightvarInteger_var1053+1
+MainProgram_rightvarInteger_var1095 = $54
+	sta MainProgram_rightvarInteger_var1095
+	sty MainProgram_rightvarInteger_var1095+1
 	lda expression+1
-	eor MainProgram_rightvarInteger_var1053+1
+	eor MainProgram_rightvarInteger_var1095+1
 	tay
 	lda expression
-	eor MainProgram_rightvarInteger_var1053
-	bcs MainProgram_wordAdd1052
+	eor MainProgram_rightvarInteger_var1095
+	bcs MainProgram_wordAdd1094
 	dey
-MainProgram_wordAdd1052
+MainProgram_wordAdd1094
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1046
-MainProgram_elsedoneblock1047
+MainProgram_elseblock1088
+MainProgram_elsedoneblock1089
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$23;keep
-	bne MainProgram_elsedoneblock1057
-MainProgram_ConditionalTrueBlock1055
+	bne MainProgram_elsedoneblock1099
+MainProgram_ConditionalTrueBlock1097
 	; Assigning single variable : expression
 	; HandleVarBinopB16bit
 	ldy #0 ; ::HandleVarBinopB16bit 0
 	lda stack
-MainProgram_rightvarInteger_var1063 = $54
-	sta MainProgram_rightvarInteger_var1063
-	sty MainProgram_rightvarInteger_var1063+1
+MainProgram_rightvarInteger_var1105 = $54
+	sta MainProgram_rightvarInteger_var1105
+	sty MainProgram_rightvarInteger_var1105+1
 	lda expression+1
 	sec
-	sbc MainProgram_rightvarInteger_var1063+1
+	sbc MainProgram_rightvarInteger_var1105+1
 	tay
 	lda expression
 	sec
-	sbc MainProgram_rightvarInteger_var1063
-	bcs MainProgram_wordAdd1062
+	sbc MainProgram_rightvarInteger_var1105
+	bcs MainProgram_wordAdd1104
 	dey
-MainProgram_wordAdd1062
+MainProgram_wordAdd1104
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1056
-MainProgram_elsedoneblock1057
+MainProgram_elseblock1098
+MainProgram_elsedoneblock1099
 	; Binary clause Simplified: EQUALS
 	; Load Byte array
 	ldx j
 	lda opcode,x
 	; Compare with pure num / var optimization
 	cmp #$1d;keep
-	bne MainProgram_elsedoneblock1067
-MainProgram_ConditionalTrueBlock1065
+	bne MainProgram_elsedoneblock1109
+MainProgram_ConditionalTrueBlock1107
 	; Assigning single variable : expression
 	ldy #0
 	; Mul 16x8 setup
@@ -576,10 +569,10 @@ MainProgram_ConditionalTrueBlock1065
 	; Calling storevariable
 	sta expression
 	sty expression+1
-MainProgram_elseblock1066
-MainProgram_elsedoneblock1067
+MainProgram_elseblock1108
+MainProgram_elsedoneblock1109
 	
-; //											Interaction			
+; //											In the meanwhile, check for key interaction 	
 	; Assigning single variable : key
 	jsr c64_getKey
 	; Calling storevariable
@@ -587,8 +580,8 @@ MainProgram_elsedoneblock1067
 	; Binary clause Simplified: EQUALS
 	; Compare with pure num / var optimization
 	cmp #$30;keep
-	bne MainProgram_elsedoneblock1073
-MainProgram_ConditionalTrueBlock1071
+	bne MainProgram_elsedoneblock1115
+MainProgram_ConditionalTrueBlock1113
 	; Assigning single variable : opcodeindex
 	lda #$0
 	; Calling storevariable
@@ -597,30 +590,30 @@ MainProgram_ConditionalTrueBlock1071
 	lda key
 	; Calling storevariable
 	sta lastkey
-MainProgram_elseblock1072
-MainProgram_elsedoneblock1073
+MainProgram_elseblock1114
+MainProgram_elsedoneblock1115
 	; Binary clause Simplified: NOTEQUALS
 	lda key
 	; Compare with pure num / var optimization
 	cmp lastkey;keep
-	beq MainProgram_elsedoneblock1079
-MainProgram_ConditionalTrueBlock1077
+	beq MainProgram_elsedoneblock1121
+MainProgram_ConditionalTrueBlock1119
 	; Binary clause: GREATER
 	lda key
 	; Compare with pure num / var optimization
 	cmp #$30;keep
 	; BC done
-	bcc MainProgram_elseblock1106
-	beq MainProgram_elseblock1106
-MainProgram_binaryclausesuccess1116
+	bcc MainProgram_elseblock1148
+	beq MainProgram_elseblock1148
+MainProgram_binaryclausesuccess1158
 	; Binary clause: LESS
 	lda key
 	; Compare with pure num / var optimization
 	cmp #$5b;keep
 	; BC done
-	bcs MainProgram_elseblock1106
-MainProgram_binaryclausesuccess1118
-MainProgram_ConditionalTrueBlock1105
+	bcs MainProgram_elseblock1148
+MainProgram_binaryclausesuccess1160
+MainProgram_ConditionalTrueBlock1147
 	; Assigning single variable : screenmemory
 	; Store Variable simplified optimization : right-hand term is pure
 	ldy opcodeindex ; optimized, look out for bugs
@@ -644,25 +637,25 @@ MainProgram_ConditionalTrueBlock1105
 	lda opcodeindex
 	; Compare with pure num / var optimization
 	cmp #$4;keep
-	bne MainProgram_elsedoneblock1123
-MainProgram_ConditionalTrueBlock1121
+	bne MainProgram_elsedoneblock1165
+MainProgram_ConditionalTrueBlock1163
 	; Assigning single variable : opcodeindex
 	lda #$0
 	; Calling storevariable
 	sta opcodeindex
-MainProgram_elseblock1122
-MainProgram_elsedoneblock1123
-MainProgram_elseblock1106
-MainProgram_elsedoneblock1107
-MainProgram_elseblock1078
-MainProgram_elsedoneblock1079
+MainProgram_elseblock1164
+MainProgram_elsedoneblock1165
+MainProgram_elseblock1148
+MainProgram_elsedoneblock1149
+MainProgram_elseblock1120
+MainProgram_elsedoneblock1121
 	inc j
 	lda #$4
 	cmp j ;keep
-	beq MainProgram_forLoopDone995
-MainProgram_forLoopNotDone996
-	jmp MainProgram_for864
-MainProgram_forLoopDone995
+	beq MainProgram_forLoopDone1037
+MainProgram_forLoopNotDone1038
+	jmp MainProgram_for906
+MainProgram_forLoopDone1037
 	; Assigning single variable : temparray
 	; Store Variable simplified optimization : right-hand term is pure
 	ldx i ; optimized, look out for bugs
@@ -677,31 +670,31 @@ MainProgram_forLoopDone995
 	clc
 	ldy #0   ; Force integer assignment, set y = 0 for values lower than 255
 	adc #$1
-	bcc MainProgram_WordAdd1126
+	bcc MainProgram_WordAdd1168
 	inc t+1
-MainProgram_WordAdd1126
+MainProgram_WordAdd1168
 	sta t+0
 	inc i
 	; Integer constant assigning
 	ldy #$01
 	lda #$00
 	cmp i ;keep
-	beq MainProgram_forLoopDone861
-MainProgram_forLoopNotDone862
-	jmp MainProgram_for595
-MainProgram_forLoopDone861
+	beq MainProgram_forLoopDone903
+MainProgram_forLoopNotDone904
+	jmp MainProgram_for637
+MainProgram_forLoopDone903
 	; Assigning single variable : k
 	lda #$0
 	; Calling storevariable
 	sta k
-MainProgram_for1127
-MainProgram_forLoopFix1147
+MainProgram_for1169
+MainProgram_forLoopFix1189
 	; Assigning single variable : j
 	lda #$0
 	; Calling storevariable
 	sta j
-MainProgram_for1151
-MainProgram_forLoopFix1159
+MainProgram_for1193
+MainProgram_forLoopFix1201
 	
 ; //											Screen write	
 	; Assigning single variable : screenmemory
@@ -716,8 +709,8 @@ MainProgram_forLoopFix1159
 	asl
 	asl
 	asl
-MainProgram_rightvarAddSub_var1164 = $54
-	sta MainProgram_rightvarAddSub_var1164
+MainProgram_rightvarAddSub_var1206 = $54
+	sta MainProgram_rightvarAddSub_var1206
 	; 8 bit binop
 	; Add/sub where right value is constant number
 	; Right is PURE NUMERIC : Is word =0
@@ -729,23 +722,23 @@ MainProgram_rightvarAddSub_var1164 = $54
 	adc #$4
 	 ; end add / sub var with constant
 	clc
-	adc MainProgram_rightvarAddSub_var1164
+	adc MainProgram_rightvarAddSub_var1206
 	asl ; *2
 	tax
 	lda saddr,x   ; Address of table lo
 	ldy saddr+1,x   ; Address of table hi
 	clc
 	adc #$4
-	bcc MainProgram_dtnooverflow1163
+	bcc MainProgram_dtnooverflow1205
 	iny  ; overflow into high byte
-MainProgram_dtnooverflow1163
+MainProgram_dtnooverflow1205
 	sta screenmemory
 	sty screenmemory+1
 	; Assigning single variable : i
 	lda #$0
 	; Calling storevariable
 	sta i
-MainProgram_for1165
+MainProgram_for1207
 	; Assigning single variable : screenmemory
 	; Load Byte array
 	; 8 bit binop
@@ -791,22 +784,137 @@ MainProgram_for1165
 	inc i
 	lda #$80
 	cmp i ;keep
-	bne MainProgram_for1165
-MainProgram_forLoopDone1167
+	bne MainProgram_for1207
+MainProgram_forLoopDone1209
 	inc j
 	lda #$2
 	cmp j ;keep
-	beq MainProgram_forLoopDone1160
-MainProgram_forLoopNotDone1161
-	jmp MainProgram_for1151
-MainProgram_forLoopDone1160
+	beq MainProgram_forLoopDone1202
+MainProgram_forLoopNotDone1203
+	jmp MainProgram_for1193
+MainProgram_forLoopDone1202
 	inc k
 	lda #$2
 	cmp k ;keep
-	beq MainProgram_forLoopDone1148
-MainProgram_forLoopNotDone1149
-	jmp MainProgram_for1127
-MainProgram_forLoopDone1148
+	beq MainProgram_forLoopDone1190
+MainProgram_forLoopNotDone1191
+	jmp MainProgram_for1169
+MainProgram_forLoopDone1190
+	; Assigning single variable : k
+	lda #$0
+	; Calling storevariable
+	sta k
+MainProgram_for1211
+MainProgram_forLoopFix1231
+	; Assigning single variable : j
+	lda #$0
+	; Calling storevariable
+	sta j
+MainProgram_for1235
+MainProgram_forLoopFix1243
+	; Assigning single variable : screenmemory
+	; ----------
+	; AddressTable address, xoffset, yoffset
+	; yoffset is complex
+	; 8 bit binop
+	; Add/sub right value is variable/expression
+	; Right is PURE NUMERIC : Is word =0
+	; 8 bit mul of power 2
+	lda k
+	asl
+	asl
+	asl
+MainProgram_rightvarAddSub_var1248 = $54
+	sta MainProgram_rightvarAddSub_var1248
+	; 8 bit binop
+	; Add/sub where right value is constant number
+	; Right is PURE NUMERIC : Is word =0
+	; 8 bit mul of power 2
+	lda j
+	asl
+	asl
+	clc
+	adc #$4
+	 ; end add / sub var with constant
+	clc
+	adc MainProgram_rightvarAddSub_var1248
+	asl ; *2
+	tax
+	lda caddr,x   ; Address of table lo
+	ldy caddr+1,x   ; Address of table hi
+	clc
+	adc #$4
+	bcc MainProgram_dtnooverflow1247
+	iny  ; overflow into high byte
+MainProgram_dtnooverflow1247
+	sta screenmemory
+	sty screenmemory+1
+	; Assigning single variable : i
+	lda #$0
+	; Calling storevariable
+	sta i
+MainProgram_for1249
+	; Assigning single variable : screenmemory
+	; Load Byte array
+	; 8 bit binop
+	; Add/sub where right value is constant number
+	; Right is PURE NUMERIC : Is word =0
+	; 8 bit mul of power 2
+	lda j
+	asl
+	asl
+	asl
+	asl
+	asl
+	asl
+	asl
+	clc
+	adc i
+	 ; end add / sub var with constant
+	tax
+	lda temparray,x
+	; Calling storevariable
+	pha
+	; 8 bit binop
+	; Add/sub where right value is constant number
+	; Right is PURE NUMERIC : Is word =0
+	; 8 bit mul of power 2
+	; Right is PURE NUMERIC : Is word =0
+	; 8 bit mul of power 2
+	lda i
+	lsr
+	lsr
+	lsr
+	lsr
+	lsr
+	asl
+	asl
+	asl
+	clc
+	adc i
+	 ; end add / sub var with constant
+	tay
+	pla
+	sta (screenmemory),y
+	inc i
+	lda #$80
+	cmp i ;keep
+	bne MainProgram_for1249
+MainProgram_forLoopDone1251
+	inc j
+	lda #$2
+	cmp j ;keep
+	beq MainProgram_forLoopDone1244
+MainProgram_forLoopNotDone1245
+	jmp MainProgram_for1235
+MainProgram_forLoopDone1244
+	inc k
+	lda #$2
+	cmp k ;keep
+	beq MainProgram_forLoopDone1232
+MainProgram_forLoopNotDone1233
+	jmp MainProgram_for1211
+MainProgram_forLoopDone1232
 	jmp MainProgram_while8
 MainProgram_elseblock10
 MainProgram_elsedoneblock11
@@ -814,4 +922,4 @@ MainProgram_elsedoneblock11
 EndSymbol
 	; End of program
 	; Ending memory block
-EndBlock127
+EndBlock3
